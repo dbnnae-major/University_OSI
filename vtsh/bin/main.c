e6fbecf81d1b0213dd7604ef79bc4323e17e299a
dbnnae <artemd321@gmail.com>
2025-10-01 00:05:42 +0300

    clone3 failed: Invalid argument

--- vtsh/bin/main.c
+++ vtsh/bin/main.c
@@ -1,9 +1,18 @@
+#include <linux/sched.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 #include <vtsh.h>
 
 #define MAX_INPUT 256
 #define MAX_ARGS 8
+#define E9 1000000000.0
+#define STACK_SIZE ((size_t)(1024 * 1024))
 
 int parse_input(char* input, char* argv[]) {
   int argc = 0;
@@ -18,6 +27,39 @@ int parse_input(char* input, char* argv[]) {
 
   argv[argc] = NULL;
   return argc;
+}
+
+void run_command(char* argv[]) {
+  struct timespec start;
+  struct timespec end;
+  struct clone_args args;
+
+  char child_stack[STACK_SIZE];
+
+  memset(&args, 0, sizeof(args));
+  args.flags = SIGCHLD;
+  args.stack = (unsigned long)(child_stack + STACK_SIZE);
+  args.stack_size = STACK_SIZE;
+  pid_t pid = -1;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  pid = (pid_t)syscall(SYS_clone3, &args, sizeof(args));
+
+  if (pid == -1) {
+    perror("clone3 failed");
+    return;
+  }
+  if (pid == 0) {
+    execvp(argv[0], argv);
+    perror("execvp failed");
+    _exit(1);
+  } else {
+    waitpid(pid, NULL, 0);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time = (double)(end.tv_sec - start.tv_sec) +
+                  (double)(end.tv_nsec - start.tv_nsec) / E9;
+    printf("Execution time: %.6f sec\n", time);
+  }
 }
 
 int main() {
@@ -45,7 +87,7 @@ int main() {
       continue;
     }
 
-    printf("%d", argc);
+    run_command(argv);
   }
 
   return 0;
