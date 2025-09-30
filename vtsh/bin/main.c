fcb6c89fb1beab4e550b48e6123d1b43f0863fd0
dbnnae <artemd321@gmail.com>
2025-09-30 23:13:13 +0300

    fix exit + parse input

--- vtsh/bin/main.c
+++ vtsh/bin/main.c
@@ -3,6 +3,22 @@
 #include <vtsh.h>
 
 #define MAX_INPUT 256
+#define MAX_ARGS 8
+
+int parse_input(char* input, char* argv[]) {
+  int argc = 0;
+
+  char* save = NULL;
+  char* frag = strtok_r(input, " ", &save);
+
+  while (frag != NULL && argc < MAX_ARGS - 1) {
+    argv[argc++] = frag;
+    frag = strtok_r(NULL, " ", &save);
+  }
+
+  argv[argc] = NULL;
+  return argc;
+}
 
 int main() {
   char input[MAX_INPUT];
@@ -17,10 +33,19 @@ int main() {
 
     input[strcspn(input, "\n")] = '\0';
 
-    if (strcspn(input, "exit") == 0) {
+    if (strcmp(input, "exit") == 0) {
       printf("%s", vtsh_exit_prompt());
       break;
     }
+
+    char* argv[MAX_ARGS];
+    int argc = parse_input(input, argv);
+
+    if (argc == 0) {
+      continue;
+    }
+
+    printf("%d", argc);
   }
 
   return 0;
