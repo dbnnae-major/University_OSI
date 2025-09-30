6da222c3178ed6d860a52b56bb8a0ee4301baf53
dbnnae <artemd321@gmail.com>
2025-09-30 22:51:25 +0300

    exit

--- vtsh/bin/main.c
+++ vtsh/bin/main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <vtsh.h>
 
+#define MAX_INPUT 256
+
 int main() {
-  printf("%s", vtsh_prompt());
-  printf("Hello, world!\n");
+  char input[MAX_INPUT];
+
+  while (1) {
+    printf("%s", vtsh_prompt());
+
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+      printf("Error of input\n");
+      break;
+    }
+
+    input[strcspn(input, "\n")] = '\0';
+
+    if (strcspn(input, "exit") == 0) {
+      printf("%s", vtsh_exit_prompt());
+      break;
+    }
+  }
 
   return 0;
 }
--- vtsh/lib/vtsh.c
+++ vtsh/lib/vtsh.c
@@ -3,3 +3,7 @@
 const char* vtsh_prompt() {
   return "vtsh> ";
 }
+
+const char* vtsh_exit_prompt() {
+  return "vtsh< ";
+}
